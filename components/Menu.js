e9baae7ae6b8184241fafbbf5186b56e3035fcbf
Ryan <snorekr@gmail.com>
2021-09-29 10:10:24 -0400

    e

--- components/Menu.js
+++ components/Menu.js
@@ -23,7 +23,6 @@ function menuMaker(items) {
   div.append(ul)
   // Step 3: Still inside your function, select from the DOM the menu button (the element with a class of 'menu-button').
   const menuButton = document.querySelector('.menu-button')
-
   // Step 4: Add a click event listener to the menu button. When clicked it should toggle the class 'menu--open' on div.menu (your div with a 'menu' class).
   menuButton.addEventListener('click', () => div.classList.toggle('menu--open'))
   // Step 5: Don't forget to return your div.menu.
